16786b09ae371f23c22dd29efe2d2fa204318f88
gayatrilangade <langadegayatri04@gmail.com>
2026-05-03 23:32:12 +0530

    Update databasesample_data.sql

---
+++ databasesample_data.sql
@@ -0,0 +1,9 @@
+
+INSERT INTO USERS VALUES (1, 'Rahul', 'rahul@gmail.com');
+
+INSERT INTO BOOKS VALUES (101, 'Java Basics', 'James Gosling', 'Programming');
+
+INSERT INTO ISSUE VALUES (201, 1, 101, SYSDATE, NULL);
+
+INSERT INTO REVIEWS VALUES (301, 1, 101, 5, 'Excellent book');
+
